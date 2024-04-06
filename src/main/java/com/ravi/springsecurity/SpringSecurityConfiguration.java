8b65be82db12c29171359f3dd0ef571b57d760d7
Ravi N <your.email@example.com>
2024-04-06 23:33:28 +0530

    jdddh

--- src/main/java/com/ravi/springsecurity/SpringSecurityConfiguration.java
+++ src/main/java/com/ravi/springsecurity/SpringSecurityConfiguration.java
@@ -1,27 +1,37 @@
 package com.ravi.springsecurity;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.sql.DataSource;
+
+import static javax.management.Query.and;
+
 @EnableWebSecurity
 public class SpringSecurityConfiguration extends WebSecurityConfiguration {
 
+    @Autowired
+    DataSource dataSource;
 
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-        auth.inMemoryAuthentication()
-                .withUser("user")
-                .password("blah")
-                .roles("USER");
+        auth.jdbcAuthentication()
+                .dataSource(dataSource);
     }
 
-    @Bean
-    public PasswordEncoder getPasswordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .requestMatchers("/admin").hasRole("ADMIN")
+                .requestMatchers("/user").hasAnyRole("ADMIN", "USER")
+                .requestMatchers("/").permitAll()
+                .and();
     }
 
 }
