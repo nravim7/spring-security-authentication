46e9eefd4512cdd31347a2c838853719b5d96a8c
Ravi N <your.email@example.com>
2024-04-07 00:25:31 +0530

    Adding resources and password encoder and updating pom

--- src/main/java/com/ravi/springsecurity/Controller.java
+++ src/main/java/com/ravi/springsecurity/Controller.java
@@ -6,8 +6,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
-    @GetMapping("/welcome")
+    @GetMapping("/")
     public String sayHello() {
         return ("<h1>Welcome</h1>");
+    }
+
+    @GetMapping("/user")
+    public String sayHelloUser() {
+        return ("<h1>Welcome User</h1>");
+    }
+
+    @GetMapping("/admin")
+    public String sayHelloAdmin() {
+        return ("<h1>Welcome Admin</h1>");
     }
 }
